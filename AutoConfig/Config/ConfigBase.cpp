65e8bb2137c42cd5e27884e3d7f5557cd7ae62b0
xiaol-luo <xiaol.luo@163.com>
2017-08-11 01:23:52 +0800

    modify auto config ' '

--- AutoConfig/Config/ConfigBase.cpp
+++ AutoConfig/Config/ConfigBase.cpp
@@ -2,10 +2,32 @@
 
 namespace Config
 {
+    static const char * Field_Name_int_val = "int_val";
+    static const char * Field_Name_float_val = "float_val";
+    static const char * Field_Name_int_vec = "int_vec";
+    static const char * Field_Name_int_int_map = "int_int_map";
+    static const char * Field_Name_int_vec_vec = "int_vec_vec";
+    static const char * Field_Name_int_float_map_vec = "int_float_map_vec";
+
     const ConfigBase & ConfigBase::Default()
     {
         static ConfigBase cfg;
         return cfg;
+    }
+
+    bool ConfigBase::Init(std::map<std::string, std::string> kvPairs, CheckLineFunc func)
+    {
+        bool all_ok = true;
+        all_ok = all_ok && kvPairs.count(Field_Name_int_val) > 0 && Str2BaseValue(kvPairs[Field_Name_int_val], int_val);
+        all_ok = all_ok && kvPairs.count(Field_Name_float_val) > 0 && Str2BaseValue(kvPairs[Field_Name_float_val], float_val);
+        all_ok = all_ok && kvPairs.count(Field_Name_int_vec) > 0 && Str2Vec(kvPairs[Field_Name_int_vec], int_vec);
+        all_ok = all_ok && kvPairs.count(Field_Name_int_int_map) > 0 && Str2Map(kvPairs[Field_Name_int_int_map], int_int_map);
+        all_ok = all_ok && kvPairs.count(Field_Name_int_vec_vec) > 0 && Str2VecVec(kvPairs[Field_Name_int_vec_vec], int_vec_vec);
+        all_ok = all_ok && kvPairs.count(Field_Name_int_float_map_vec) > 0 && Str2MapVec(kvPairs[Field_Name_int_float_map_vec], int_float_map_vec);
+        all_ok = all_ok && extra_data.Init(*this);
+        if (all_ok && nullptr != func)
+            all_ok &= func(this);
+        return all_ok;
     }
 }
 
--- AutoConfig/main.cpp
+++ AutoConfig/main.cpp
@@ -40,13 +40,20 @@ int main(int argc, char **argv)
     std::map<int, std::vector<std::string>> int_str_map_vec;
     ret = Config::Str2MapVec("1:a,b,c;2:d,e,f;3:sdf,sfsf", int_str_map_vec);
 
+    static const char * Field_Name_int_val = "int_val";
+    static const char * Field_Name_float_val = "float_val";
+    static const char * Field_Name_int_vec = "int_vec";
+    static const char * Field_Name_int_int_map = "int_int_map";
+    static const char * Field_Name_int_vec_vec = "int_vec_vec";
+    static const char * Field_Name_int_float_map_vec = "int_float_map_vec";
+
     std::map<std::string, std::string> data;
-    data[Config::ConfigBase::Default().Field_Name_int_val] = "1024";
-    data[Config::ConfigBase::Default().Field_Name_float_val] = "1234";
-    data[Config::ConfigBase::Default().Field_Name_int_vec] = "1;2;3;4;5";
-    data[Config::ConfigBase::Default().Field_Name_int_int_map] = "1:2;2:3";
-    data[Config::ConfigBase::Default().Field_Name_int_vec_vec] = "1,2,3;4,5,6";
-    data[Config::ConfigBase::Default().Field_Name_int_float_map_vec] = "1:100,101,102; 2:202,203";
+    data[Field_Name_int_val] = "1024";
+    data[Field_Name_float_val] = "1234";
+    data[Field_Name_int_vec] = "1;2;3;4;5";
+    data[Field_Name_int_int_map] = "1:2;2:3";
+    data[Field_Name_int_vec_vec] = "1,2,3;4,5,6";
+    data[Field_Name_int_float_map_vec] = "1:100,101,102; 2:202,203";
     ret = cfg_base.Init(data, ConfigBaseChecLine);
     printf("hello world");
 }
