3d8f42d78497dea97e90d9f782bedd53493510d1
xiaol-luo <xiaol.luo@163.com>
2017-08-13 19:50:50 +0800

    modify auto config

--- AutoConfig/main.cpp
+++ AutoConfig/main.cpp
@@ -3,6 +3,10 @@
 #include "csv_config_sets.h"
 #include "IniFile/IniFile.hpp"
 #include <sstream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidxml/rapidxml.hpp"
 
 bool ConfigBaseChecLine(void* cfg)
 {
@@ -19,4 +23,10 @@ int main(int argc, char **argv)
     std::istringstream ss(("[Foo]\nbar=hello world\n[Test]"));
     ini::IniFile inif(ss);
     std::string ini_str = inif["Foo"]["bar"].asString();
+
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    rapidjson::Document d;
+    d.Parse(json);
+
+    rapidxml::xml_document<> xml_doc;
 }
